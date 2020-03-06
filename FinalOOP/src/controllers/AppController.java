8e3b5d345def5e4f73323e6f97028f56b5b24b29
CodySullivan <61592491+CodySullivan@users.noreply.github.com>
2020-03-06 11:29:41 -0700

    Stuff Updated
    Updated Stuff

--- FinalOOP/src/controllers/AppController.java
+++ FinalOOP/src/controllers/AppController.java
@@ -1,17 +1,25 @@
 package controllers;
 
+import models.*;
+
 public class AppController {
     public static final String Pink = "\033[0;35m";
     public static final String Red = "\033[0;31m";
 
-    public static void Doors(){
 
+    public static void test1(){
+        Brick brick = new Brick();
+        Chainsaw chainsaw = new Chainsaw();
+        Cleaver cleaver = new Cleaver();
+        DiamondSword diamondSword = new DiamondSword();
+        Hatchet hatchet = new Hatchet();
+        Machete machete = new Machete();
+        RayGun rayGun = new RayGun();
     }
-    public static void Windows(){
 
-    }
-    public static void currentPosition(){
-
+    public static void currentPosition(int startPosition){
+        int position = startPosition;
+        
     }
     public static void winCondition(){
 
@@ -19,7 +27,7 @@ public class AppController {
     public static void loseCondition(){
 
     }
-    public static void KillScene(int weapon){
+    public static void KillScene(){
 
     }
 }
--- FinalOOP/src/views/View.java
+++ FinalOOP/src/views/View.java
@@ -1,12 +1,35 @@
 package views;
 
-import models.CrunchyTime;
+import controllers.AppController;
+import lib.ConsoleIO;
 
 public class View {
     public static final String Pink = "\033[0;35m";
     public static final String Red = "\033[0;31m";
-
     public static void run() {
-        CrunchyTime.time(1200);
+        AppController.test1();
+        int choice = -1;
+        do{
+            System.out.println(Pink + "Welcome to Pork, the popular console game about " + Red +"Pig cannibalism!");
+            System.out.println(Pink + "Please make a selection on what mode you want to play.");
+            System.out.println("\n");
+            String[] modes = {"Piglet Mode", "(WIP) Piggy Mode", "(WIP) Boar Mode"};
+            int choiceMode = ConsoleIO.promptForMenuSelection(modes, true);
+            System.out.println("You have chosen " + (choiceMode == 0 ? "No Mode, Kevin is going after you now" : modes[choiceMode - 1]) + "!");
+            if(choiceMode == 1){
+                System.out.println("Welcome to Piglet Mode, the easiest of all Pig Modes.");
+                System.out.println("\nLet us give a little back story. Your brother, Bacon, escaped your grasp in the last battle you both had and fled to an abandon looking building." +
+                        "\n" + "Unfortunately, Bacon ran into the only door that can lock from behind! You did find an entrance to the building... on the opposite side!" + "\n" + "You " +
+                        "are now inside the building, you look around and see that... " + Red + "There are bodies of animals handing from their backs!" + Pink + " You realize that " +
+                        "you are in an abandoned " + Red + "BUTCHER SHOP!" + "\n" + Pink + "You also know that Bacon and you are not the only living things inside...");
+            }else if(choiceMode == 2){
+                System.out.println("Welcome to Piggy Mode, the medium-est of the Pig Modes.");
+                System.out.println("This mode is not yet ready to be played. This was your mistake for picking this mode and now you shall face the consequences!");
+            }else if(choiceMode == 3){
+                System.out.println("Welcome to Boar Mode, the hardest Pig Mode of all the land!");
+                System.out.println("This mode is not yet ready to be played. This was your mistake for picking this mode and now you shall face the consequences!");
+            }
+        }while(choice > 0);
+        System.out.println("\nThe 'Game' has ended. But your life is now ready... Kevin is coming, you better run!");
     }
 }
